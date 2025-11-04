28b76c7f35ade5ea8eabc3c4e1400427bd40cc68
celine <celine.himbaza@gmail.com>
2025-11-04 15:21:06 +0200

    react-app

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', background: '#333', color: '#fff', marginTop: '20px' }}>
+      &copy; 2025 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
