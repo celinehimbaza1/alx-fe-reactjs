e58d0e765e78b2790eab2737abe05bad6efe7ce0
celine <celine.himbaza@gmail.com>
2025-11-04 11:01:21 +0200

    Add Counter component using useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,14 @@
-// src/App.jsx
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
     </div>
   );
