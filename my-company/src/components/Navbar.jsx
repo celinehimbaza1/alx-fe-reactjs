310852722fa71cdf5e3ec75b6cd23360ed89d8ad
celine <celine.himbaza@gmail.com>
2025-11-04 15:53:31 +0200

    react-app

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
+    <nav style={{
+      padding: '10px',
+      backgroundColor: '#333',
+      color: '#fff',
+      display: 'flex',
+      justifyContent: 'flex-start'
+    }}>
       <Link to="/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
