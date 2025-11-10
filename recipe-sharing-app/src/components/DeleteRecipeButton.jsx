65fa226c74af98399f37b0b41f679ba43654b9fa
celine <celine.himbaza@gmail.com>
2025-11-10 14:49:57 +0200

    state management

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,17 @@
+import { useRecipeStore } from "./recipeStore";
+
+const DeleteRecipeButton = ({ recipeId }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+  };
+
+  return (
+    <button onClick={handleDelete}>
+      Delete
+    </button>
+  );
+};
+
+export default DeleteRecipeButton;
