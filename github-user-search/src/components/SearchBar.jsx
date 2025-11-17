dc12a42534291fb7513582fb56d0fedfcba63541
celine <celine.himbaza@gmail.com>
2025-11-17 18:06:12 +0200

    githubusersearch

---
+++ github-user-search/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+function SearchBar() {
+  return (
+    <input
+      type="text"
+      placeholder="Search GitHub user..."
+      className="border p-2 rounded w-full"
+    />
+  );
+}
+
+export default SearchBar;
