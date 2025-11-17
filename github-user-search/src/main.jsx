935af50cdeb3d62e40d18f4daafc7d4909699590
celine <celine.himbaza@gmail.com>
2025-11-17 18:31:09 +0200

    githubusersearch

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,10 +1,9 @@
-// src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // optional
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
