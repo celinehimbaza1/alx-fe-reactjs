ac038be24132ffaf07ea8dd3f4cb33976375d068
celine <celine.himbaza@gmail.com>
2025-11-17 18:36:37 +0200

    githubusersearch

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,22 @@
+import axios from "axios";
+
+// Basic single user fetch (Task 1)
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
+
+// Advanced search (Task 2)
 export const advancedSearchUsers = async (username, location, minRepos) => {
   let query = "";
-
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos} `;
 
-  const response = await fetch(
+  const response = await axios.get(
     `https://api.github.com/search/users?q=${query.trim()}`
   );
 
-  if (!response.ok) {
-    throw new Error("API error");
-  }
-
-  const data = await response.json();
-  return data.items || [];
+  if (!response.data.items) return [];
+  return response.data.items;
 };
