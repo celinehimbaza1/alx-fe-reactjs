d208b799286280a9902d508d6ce478dfca7c94e6
celine <celine.himbaza@gmail.com>
2025-11-26 11:37:29 +0200

    tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
 
       <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center transition-colors duration-300 hover:text-blue-500">
